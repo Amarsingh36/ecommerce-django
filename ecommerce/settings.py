ebf0310649718b3d35dc62aef28d28d144398aa4
Amarsingh36 <amar.2s9845@gmail.com>
2025-04-05 21:15:09 +0530

    Configured static files and whitenoise for deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,3 +138,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
---
+++ staticfiles/store/js/cart.js
@@ -0,0 +1,74 @@
+var updateBtns = document.getElementsByClassName('update-cart');
+
+for (var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        var productId = this.dataset.product;
+        var action = this.dataset.action;
+        console.log('productId:', productId, 'action:', action);
+
+        console.log('USER:', user);
+        if (user === 'AnonymousUser') {
+            addCookieItem(productId, action);
+        } else {
+            updateUserOrder(productId, action);
+        }
+    });
+}
+
+function updateUserOrder(productId, action) {
+    console.log('User is logged in, sending data..');
+
+    var url = '/update_item/';
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({ 'productId': productId, 'action': action })
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('data:', data);
+            location.reload();
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function addCookieItem(productId, action) {
+    console.log('User not logged in...');
+
+    let cart = JSON.parse(getCookie('cart') || '{}');
+
+    if (action == 'add') {
+        if (cart[productId] == undefined) {
+            cart[productId] = { 'quantity': 1 };
+        } else {
+            cart[productId]['quantity'] += 1;
+        }
+    }
+
+    if (action == 'remove') {
+        cart[productId]['quantity'] -= 1;
+
+        if (cart[productId]['quantity'] <= 0) {
+            delete cart[productId];
+        }
+    }
+
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/";
+    console.log('Cart:', cart);
+    location.reload();
+}
+
+function getCookie(name) {
+    let cookieArr = document.cookie.split(';');
+    for (let i = 0; i < cookieArr.length; i++) {
+        let cookiePair = cookieArr[i].split('=');
+        if (name == cookiePair[0].trim()) {
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    return null;
+}
